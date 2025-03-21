9b0922474baeeafca2272f63a21eac75ca3306cd
parisollie <pjff18@gmail.com>
2025-03-21 00:17:21 -0600

    Initial Commit

---
+++ AnimationChallengue/AnimationChallengueApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationChallengueApp.swift
+//  AnimationChallengue
+//
+//  Created by Paul F on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationChallengueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationChallengue/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  AnimationChallengue
+//
+//  Created by Paul F on 21/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+#Preview {
+    ContentView()
+}
